8da8aa513834b7a2e4bdb0d48a1a28b1c85a3330
Rafael Borges <rafaelbertelli@gmail.com>
2019-05-26 21:19:30 -0300

    Config redux store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Provider store={store}>
+      <h1>Hello world</h1>;
+    </Provider>
+  );
 }
 
 export default App;
